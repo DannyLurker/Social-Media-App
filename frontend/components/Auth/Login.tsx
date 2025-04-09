d0b136010a7b10d70045a58129698a1d1f7127bb
DannyLurker <dannycode992@gmail.com>
2025-04-09 16:28:43 +0700

    feat: Connect authentication UI to backend API for signup and login functionality

--- frontend/components/Auth/Login.tsx
+++ frontend/components/Auth/Login.tsx
@@ -1,14 +1,46 @@
 "use client";
-import React, { useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import Image from "next/image";
 import PasswordInput from "./PasswordInput";
 import LoadingButton from "../helper/LoadingButton";
 import Link from "next/link";
+import axios from "axios";
+import { BASE_API_URL } from "@/server";
+import { handleAuthRequest } from "../utils/apiRequest";
+import { toast } from "sonner";
 
-// Kegunaan grid cols pada project ini untuk breakpoint (responsive), jadi ketika berada di breakpoint dibawah lg hanya hanya akan menampilkan 1 column(vertikal), jika diatas menampilkan 7 column, lalu kegunaan col span itu seperti menentukan berapa column yang akan di gunakan pada breakpoint dibawah lg, col span pada banner akan hilang(hidden) jadi nya hanya menampilkan form, dan jika diatas breakpoint lg, banner akan menggunakan 4 column lalu form akan menggunakan 3 column
+interface FormData {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const signupReq = async () =>
+      await axios.post(`${BASE_API_URL}/users/login`, formData, {
+        withCredentials: true,
+      });
+
+    const result = await handleAuthRequest(signupReq, setIsLoading);
+
+    if (result) {
+      console.log("test:", result);
+      toast.success(result.data.message);
+    }
+  };
 
   return (
     <div className="w-full h-screen overflow-hidden">
@@ -26,9 +58,12 @@ const Login = () => {
         {/* form */}
         <div className="lg:col-span-3 flex flex-col items-center justify-center h-screen">
           <h1 className="font-bold text-xl sm:text-2xl text-left uppercase mb-8">
-            Sign Up with <span className="text-blue-400">Nexora</span>
+            Login with <span className="text-blue-400">Nexora</span>
           </h1>
-          <form className="block w-[90%] sm:w-[80%] md:w-[90%] lg:w-[90%] xl:w-[80%]">
+          <form
+            onSubmit={handleSubmit}
+            className="block w-[90%] sm:w-[80%] md:w-[90%] lg:w-[90%] xl:w-[80%]"
+          >
             <div className="mb-4">
               <label htmlFor="email" className="font-semibold mb-2 block">
                 Email
@@ -39,6 +74,8 @@ const Login = () => {
                 id="email"
                 placeholder="Email address"
                 className="px-4 py-3 bg-gray-200 rounded-lg w-full block outline-none"
+                value={formData.email}
+                onChange={handleChange}
               />
             </div>
             <div className="mb-4">
@@ -46,6 +83,8 @@ const Login = () => {
                 label="Password"
                 name="password"
                 placeholder="Enter password"
+                value={formData.password}
+                onChange={handleChange}
               />
             </div>
             <LoadingButton
@@ -54,11 +93,11 @@ const Login = () => {
               type="submit"
               isLoading={isLoading}
             >
-              Sign Up Now
+              Login Now
             </LoadingButton>
           </form>
           <p className="mt-4 text-lg text-gray-800 text-center">
-            Already have an account ?{" "}
+            You haven't had an account yet ?{" "}
             <Link href={"/auth/signup"}>
               <span className="text-blue-500 hover:text-blue-600 underline cursor-pointer font-bold">
                 Create an account
